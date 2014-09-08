cd4732f17f911b76913c00613146b5115d11b72b
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-09-08 11:12:27 +0100

    Update page.php

--- page.php
+++ page.php
@@ -7,15 +7,7 @@
 			<?php while (have_posts()) : the_post(); // Start loop ?>
 				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 		      <?php	edit_post_link( __( '<span class="button success">Edit this page</span>', 'fst' ), '<span class="edit-link">', '</span>' ); ?>
-					<header>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header>
-					<div class="entry-content">
-						<?php the_content(); ?>
-					</div>
-					<footer>
-						<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'fst'), 'after' => '</p></nav>' )); ?>
-					</footer>
+					<?php get_template_part( 'structure/content/content', 'page' ); ?>
 				</article>
 			<?php endwhile; // End loop ?>
 		</section>
