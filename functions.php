1c66d9bfe9e900fab0ce8cd681a345f62f250dfa
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-09-08 11:19:16 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
     require_once('functions/cleanup.php');
     require_once('functions/enqueues.php');
     require_once('functions/foundation.php');
-    require_once('functions/top-bar_off-canvas-sidebar.php');
+    require_once('functions/menus.php');
     require_once('functions/widgets.php');
     require_once('functions/search-form.php');
     require_once('functions/theme-supports.php');
