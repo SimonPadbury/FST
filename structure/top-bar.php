f47b003604d75ef4f79ed0d85ff9cef489aefbbb
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-10-24 11:12:17 +0100

    Update top-bar.php

--- structure/top-bar.php
+++ structure/top-bar.php
@@ -1,29 +1,28 @@
-<section class="top-bar-section">
-    <?php
-        wp_nav_menu( array(
-            'theme_location' => 'top-bar-left',
-            'container' => false,
-            'depth' => 0,
-            'items_wrap' => '<ul class="left">%3$s</ul>',
-            'fallback_cb' => 'fst_menu_fallback', // Show fallback message if no menus
-            'walker' => new fst_walker( array(
-              'in_top_bar' => true,
-              'item_type' => 'li',
-              'menu_type' => 'main-menu'
-            ) ),
-        ) );
-    ?>
-    <?php
-        wp_nav_menu( array(
-            'theme_location' => 'top-bar-right',
-            'container' => false,
-            'depth' => 0,
-            'items_wrap' => '<ul class="right">%3$s</ul>',
-            'walker' => new fst_walker( array(
-            'in_top_bar' => true,
-            'item_type' => 'li',
-            'menu_type' => 'main-menu'
-            ) ),
-        ) );
-    ?>
-</section>
+
+<?php
+    wp_nav_menu( array(
+        'theme_location' => 'top-bar-left',
+        'container' => false,
+        'depth' => 0,
+        'items_wrap' => '<ul class="left">%3$s</ul>',
+        'fallback_cb' => 'fst_menu_fallback', // Show fallback message if no menus
+        'walker' => new fst_walker( array(
+          'in_top_bar' => true,
+          'item_type' => 'li',
+          'menu_type' => 'main-menu'
+        ) ),
+    ) );
+?>
+<?php
+    wp_nav_menu( array(
+        'theme_location' => 'top-bar-right',
+        'container' => false,
+        'depth' => 0,
+        'items_wrap' => '<ul class="right">%3$s</ul>',
+        'walker' => new fst_walker( array(
+        'in_top_bar' => true,
+        'item_type' => 'li',
+        'menu_type' => 'main-menu'
+        ) ),
+    ) );
+?>
