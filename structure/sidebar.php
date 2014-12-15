1fc40b95caa520259972bb27948d78f73206742b
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-15 13:18:25 +0000

    Update sidebar.php

--- structure/sidebar.php
+++ structure/sidebar.php
@@ -1,1 +1,25 @@
 <?php dynamic_sidebar("sidebar-widgets"); ?>
+
+<?php if ( ! dynamic_sidebar("sidebar-widgets") ) : ?>
+
+  <div id="search" class="widget-container widget_search">
+    <?php get_search_form(); ?>
+  </div>
+
+	<div id="archives" class="widget-container">
+    <h4 class="widget-title"><?php _e( 'Archives', 'fst' ); ?></h4>
+    <ul>
+      <?php wp_get_archives( 'type=monthly' ); ?>
+    </ul>
+	</div>
+
+	<div id="meta" class="widget-container">
+    <h4 class="widget-title"><?php _e( 'Meta', 'fst' ); ?></h4>
+    <ul>
+      <?php wp_register(); ?>
+      <li><?php wp_loginout(); ?></li>
+      <?php wp_meta(); ?>
+    </ul>
+	</div>
+
+<?php endif; ?>
