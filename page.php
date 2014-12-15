343331fd3f836882aa1943ed54c48ae5e168e64f
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-15 13:26:48 +0000

    Update page.php

--- page.php
+++ page.php
@@ -6,7 +6,6 @@
 		<section class="medium-8 columns" id="content" role="main">
 			<?php while (have_posts()) : the_post(); // Start loop ?>
 				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-		      <?php	edit_post_link( __( '<span class="button success">Edit this page</span>', 'fst' ), '<span class="edit-link">', '</span>' ); ?>
 					<?php get_template_part( 'structure/content/content', 'page' ); ?>
 				</article>
 			<?php endwhile; // End loop ?>
