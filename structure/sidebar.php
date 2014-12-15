a2c37210f8b15cb638b661e0c26a5d38e41e58c4
SimonPadbury <simon_padbury@yahoo.co.uk>
2014-12-15 13:54:59 +0000

    Update sidebar.php

--- structure/sidebar.php
+++ structure/sidebar.php
@@ -1,25 +1,5 @@
 <?php dynamic_sidebar("sidebar-widgets"); ?>
 
 <?php if ( ! dynamic_sidebar("sidebar-widgets") ) : ?>
-
-  <div id="search" class="widget-container widget_search">
-    <?php get_search_form(); ?>
-  </div>
-
-	<div id="archives" class="widget-container">
-    <h4 class="widget-title"><?php _e( 'Archives', 'fst' ); ?></h4>
-    <ul>
-      <?php wp_get_archives( 'type=monthly' ); ?>
-    </ul>
-	</div>
-
-	<div id="meta" class="widget-container">
-    <h4 class="widget-title"><?php _e( 'Meta', 'fst' ); ?></h4>
-    <ul>
-      <?php wp_register(); ?>
-      <li><?php wp_loginout(); ?></li>
-      <?php wp_meta(); ?>
-    </ul>
-	</div>
-
+	<?php echo $link = $link_before . '<div class="alert-box info"><a href="' .admin_url( 'widgets.php' ) . '">' . $before . 'Add some widgets' . $after . '</a></div>' . $link_after; ?>
 <?php endif; ?>
