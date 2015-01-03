094e89bbfb465e701f64b208c31367701e71e3a6
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-03 18:16:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php get_template_part('includes/header'); ?>
 
 <section class="main-section">
@@ -16,37 +15,3 @@
 </section><!-- /.main-section -->
 
 <?php get_template_part('includes/footer'); ?>
-=======
-<?php get_template_part('structure/header'); ?>
-
-<div class="container" role="document">
-	<div class="row">
-
-		<section class="medium-8 columns" id="content" role="main">
-
-			<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'structure/content/content', get_post_format() ); ?>
-				<?php endwhile; ?>
-				<?php else : ?>
-					<?php get_template_part( 'structure/content/content', 'none' ); ?>
-			<?php endif;?>
-
-			<?php if ( function_exists('fst_pagination') ) { fst_pagination(); } else if ( is_paged() ) { ?>
-				<nav id="post-nav">
-					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'fst' ) ); ?></div>
-					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'fst' ) ); ?></div>
-				</nav>
-			<?php } ?>
-
-		</section>
-
-		<aside id="sidebar" class="medium-4 columns">
-			<?php get_template_part('structure/sidebar'); ?>
-		</aside>
-
-	</div>
-</div>
-
-<?php get_template_part('structure/footer'); ?>
->>>>>>> FETCH_HEAD
