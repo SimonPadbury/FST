f94bd9db885ef2e2b45445ad06ea3f8c1b337bbe
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-03 18:11:55 +0000

    Update menus.php

--- functions/menus.php
+++ functions/menus.php
@@ -1,7 +1,6 @@
 <?php
 
 // Register Top-Bar and Off-Canvas Sidebar menus
-<<<<<<< HEAD
 
 register_nav_menus(
 	array(
@@ -13,18 +12,6 @@ register_nav_menus(
 
 // Foundation Top-Bar extension for WordPress Walker Nav-Menu
 
-=======
-register_nav_menus(
-	array(
-		'top-bar-left' => __('Top-bar Left', 'fst'),
-		'top-bar-right' => __('Top-bar Right', 'fst'),
-		'off-canvas-primary' => __('Off-canvas Primary', 'fst'),
-		'off-canvas-secondary' => __('Off-canvas Secondary', 'fst'),
-	)
-);
-
-// Foundation Top-Bar extension for WordPress Walker Nav-Menu
->>>>>>> FETCH_HEAD
 class fst_walker extends Walker_Nav_Menu {
 	function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
 		$element->has_children = !empty( $children_elements[$element->ID] );
@@ -37,7 +24,6 @@ class fst_walker extends Walker_Nav_Menu {
 		parent::start_el( $item_html, $object, $depth, $args );
 		$output .= ( $depth == 0 ) ? '<li class="divider"></li>' : '';
 		$classes = empty( $object->classes ) ? array() : (array) $object->classes;  
-<<<<<<< HEAD
       
 		if( in_array('label', $classes) ) {
 			$output .= '<li class="divider"></li>';
@@ -48,18 +34,6 @@ class fst_walker extends Walker_Nav_Menu {
 			$item_html = preg_replace( '/<a[^>]*>( .* )<\/a>/iU', '', $item_html );
 		}
       
-=======
-      
-		if( in_array('label', $classes) ) {
-			$output .= '<li class="divider"></li>';
-			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '<label>$1</label>', $item_html );
-		}
-      
-		if ( in_array('divider', $classes) ) {
-			$item_html = preg_replace( '/<a[^>]*>( .* )<\/a>/iU', '', $item_html );
-		}
-      
->>>>>>> FETCH_HEAD
 		$output .= $item_html;
 	}
     
@@ -68,20 +42,7 @@ class fst_walker extends Walker_Nav_Menu {
 	}
 }
 
-<<<<<<< HEAD
 // Add Foundation 'active' class for the top-bar current menu item
-=======
-// Add Foundation 'active' class for the top-bar current menu item (blue background item)
-if( ! function_exists( 'fst_active_nav_class' ) ) {
-	function fst_active_nav_class( $classes, $item ) {
-		if ( $item->current == 1 || $item->current_item_ancestor == true ) {
-			$classes[] = 'active';
-		}
-		return $classes;
-	}
-}
-add_filter( 'nav_menu_css_class', 'fst_active_nav_class', 10, 2 );
->>>>>>> FETCH_HEAD
 
 if( ! function_exists( 'fst_active_nav_class' ) ) {
 	function fst_active_nav_class( $classes, $item ) {
