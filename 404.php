3dbc1c87c41fbe2c8ec9bfe6e291ccb21c5b7863
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-03 18:15:55 +0000

    Update 404.php

--- 404.php
+++ 404.php
@@ -17,21 +17,4 @@
   </div>
 </section><!-- /.main-section -->
 
-<<<<<<< HEAD
 <?php get_template_part('includes/footer'); ?>
-=======
-		<section class="medium-8 columns" id="content" role="main">
-
-			<?php get_template_part('structure/content/content', 'none');?>
-
-		</section>
-
-		<aside id="sidebar" class="medium-4 columns">
-			<?php get_template_part('structure/sidebar'); ?>
-		</aside>
-
-	</div>
-</div>
-
-<?php get_template_part('structure/footer'); ?>
->>>>>>> FETCH_HEAD
