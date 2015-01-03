5ebd250881a1804ee3e07cd8492f32ccadbe8222
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-03 18:18:40 +0000

    Update page.php

--- page.php
+++ page.php
@@ -14,23 +14,4 @@
   </div>
 </section><!-- /.main-section -->
 
-<<<<<<< HEAD
 <?php get_template_part('includes/footer'); ?>
-=======
-		<section class="medium-8 columns" id="content" role="main">
-			<?php while (have_posts()) : the_post(); // Start loop ?>
-				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-					<?php get_template_part( 'structure/content/content', 'page' ); ?>
-				</article>
-			<?php endwhile; // End loop ?>
-		</section>
-
-		<aside id="sidebar" class="medium-4 columns">
-			<?php get_template_part('structure/sidebar'); ?>
-		</aside>
-
-	</div>
-</div>
-
-<?php get_template_part('structure/footer'); ?>
->>>>>>> FETCH_HEAD
