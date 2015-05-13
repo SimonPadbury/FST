044bd1dc4cd61f0394c8bb260485776cdaf1ef6a
SimonPadbury <simonpadbury@SimonPaburysMBP.default>
2015-05-13 08:30:00 +0100

    Update Foundation 5.5.2

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -4,10 +4,10 @@ function fst_enqueues() {
 
   // Register and enqueue stylesheets
 
-  wp_register_style('normalize-css', get_template_directory_uri() . '/css/normalize.css', false, '3.0.2', null);
+  wp_register_style('normalize-css', get_template_directory_uri() . '/css/normalize.css', false, '3.0.3', null);
 	wp_enqueue_style('normalize-css');
 
-  wp_register_style('foundation-css', get_template_directory_uri() . '/css/foundation.min.css', false, '5.5.1', null);
+  wp_register_style('foundation-css', get_template_directory_uri() . '/css/foundation.min.css', false, '5.5.2', null);
 	wp_enqueue_style('foundation-css');
 
   wp_register_style('foundation-icons-css', get_template_directory_uri() . '/fonts/foundation-icons.css', false, '3.0', null);
@@ -18,7 +18,7 @@ function fst_enqueues() {
 
   // Register and enqueue JavaScripts
 
-	wp_register_script('jquery', get_bloginfo('template_url').'/js/vendor/jquery.js', false, '2.1.3', true);
+	wp_register_script('jquery', get_bloginfo('template_url').'/js/vendor/jquery.js', false, '2.1.4', true);
 	wp_enqueue_script( 'jquery' );
 
   wp_register_script('modernizr', get_template_directory_uri() . '/js/vendor/modernizr.js', false, null, true);
